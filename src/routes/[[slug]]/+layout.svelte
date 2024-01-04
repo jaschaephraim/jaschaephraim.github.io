73324d8cd6c352dda4ee25ff55ef1a6b013dff9a
Jascha Ephraim <jaschaephraim@gmail.com>
2024-01-04 11:43:16 -0800

    better responsive layout

--- src/routes/[[slug]]/+layout.svelte
+++ src/routes/[[slug]]/+layout.svelte
@@ -4,17 +4,15 @@
   import { page } from '$app/stores';
 </script>
 
-<div class="markdown relative">
-  {#key $page.params.slug}
-    <div transition:fade={{ duration: 100 }} class="absolute">
-      {#if $page.params.slug === undefined || $page.error !== null}
-        <div class="flex flex-col md:h-[280px]">
-          <div class="grow"></div>
-          <slot />
-        </div>
-      {:else}
+{#key $page.params.slug}
+  <div transition:fade={{ duration: 100 }}>
+    {#if $page.params.slug === undefined || $page.error !== null}
+      <div class="flex flex-col md:h-[280px]">
+        <div class="grow"></div>
         <slot />
-      {/if}
-    </div>
-  {/key}
-</div>
+      </div>
+    {:else}
+      <slot />
+    {/if}
+  </div>
+{/key}
